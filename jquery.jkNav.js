b4d3de4924adaed5fe6ccb6cf28f7894dae77a14
bruno049 <j.bruno.che@gmail.com>
2014-05-05 21:04:21 -0500

    stop saving data to body, store in jQuery.jkNavData

--- jquery.jkNav.js
+++ jquery.jkNav.js
@@ -1,76 +1,90 @@
 jQuery.jkNav=function(options) {
- if($.isPlainObject(options)) {
-  if(!options['up']) {
-   options.up=75;
-  }
-  if(!options['down']) {
-   options.down=74;
-  }
-  if(!options['padding']) {
-   options.padding=0;
-  }
-  if(!options['duration']) {
-   options.duration=100;
-  }
-  if(!options['complete']) {
-   options.complete=function(){return null;};
-  }
-  options.down=parseFloat(options.down);
-  options.up=parseFloat(options.up);
-  jQuery('body').data('jkNav', options);
- } else {
-  jQuery('body').data('jkNav', {selector:options, down:74, up:75, padding:0, duration:100, complete:function(){return null;}});
- }
- /* options={
-             selector: '#blogdesc, #blogtitle, .post.text',
-  		 up: 75,
-			 down: 74,
-			 padding: 0,
+	jQuery.jkNavData={};
+	if($.isPlainObject(options)) {
+		jQuery.jkNavData.settings=$.extend(options, {down:74, up:75, padding:0, duration:100, complete:function(){return null;}};
+	} else {
+		jQuery.jkNavData.settings={selector:options, down:74, up:75, padding:0, duration:100, complete:function(){return null;}};
+	}
+	/*
+		options={
+			selector: '#blogdesc, #blogtitle, .post.text',
+			up: 75,
+			down: 74,
+			padding: 0,
+			duration:100,
+			complete:function(){};
+		}
+	*/
+	jQuery(document).keydown(function(e) {
+		if(jQuery(document.activeElement).not('body').length==0 && jQuery.jkNavData.run==true) {
+			jQuery.jkNavData.run=true;
+		} else if(jQuery(document.activeElement).not('body').length!=0 && jQuery.jkNavData.run==true){
+			jQuery.jkNavData.run=false;
+			jQuery.jkNavData.reason="input";
+		} else if(jQuery.jkNavData.run==false && jQuery(document.activeElement).not('body').length==0) {
+			if(jQuery.jkNavData.reason=="input") {
+				jQuery.jkNavData.run=true;
+				jQuery.jkNavData.reason="";
 			}
- */
- jQuery(document).keydown(function(e) {
-  if(jQuery(document.activeElement).not('body').length==0 && jQuery('body').data('jkNavtf')==true) {
-   jQuery('body').data('jkNavtf', true);
-  } else if(jQuery(document.activeElement).not('body').length!=0 && jQuery('body').data('jkNavtf')==true){
-   jQuery('body').data('jkNavtf', false);
-   jQuery('body').data('jkNavre', "input");
-  } else if(jQuery('body').data('jkNavtf')==false && jQuery(document.activeElement).not('body').length==0) {
-   if(jQuery('body').data('jkNavre')=="input") {
-    jQuery('body').data('jkNavtf', true);
-	jQuery('body').data('jkNavre', "");
-   }
-  }
-  if(jQuery('body').data('jkNavtf')==true) {
-   if(e.which==jQuery('body').data('jkNav').up || e.which==jQuery('body').data('jkNav').down) {
-    jQuery('body').data('inPort', {aLength:jQuery(jQuery('body').data('jkNav').selector).length});
-    jQuery(jQuery('body').data('jkNav').selector).each(function(i) {
-     if((jQuery(document).scrollTop()-Math.round(jQuery(this).offset().top))<(0-jQuery('body').data('jkNav').padding)) {
-	  jQuery('body').data('inPort', {exact:false, element:jQuery(this), index:i, aLength:jQuery('body').data('inPort').aLength});
-	  return false;
-	 } else if((jQuery(document).scrollTop()-Math.round(jQuery(this).offset().top))==(0-jQuery('body').data('jkNav').padding)) {
-	  jQuery('body').data('inPort', {exact:true, element:jQuery(this), index:i, aLength:jQuery('body').data('inPort').aLength});
-	  return false;
-	 }
-    });
-    switch(e.which) {
-     case jQuery('body').data('jkNav').up:
-	  if(jQuery('body').data('inPort').index!=0) {
-	   jQuery('body').data('jkNavtf', false);
-	   jQuery('body, html').animate({scrollTop:(Math.round(jQuery(jQuery('body').data('jkNav').selector).eq(jQuery('body').data('inPort').index-1).offset().top)-jQuery('body').data('jkNav').padding)}, {'duration': jQuery('body').data('jkNav').duration, 'easing':'swing', 'complete':function(){jQuery('body').data('jkNavtf', true); jQuery('body').data('jkNav').complete();}});
-	  }
-	 break;
-	 case jQuery('body').data('jkNav').down:
-	  if((jQuery('body').data('inPort').aLength-1)!=jQuery('body').data('inPort').index && jQuery('body').data('inPort').exact==true) {
-	   jQuery('body').data('jkNavtf', false);
-	   jQuery('body, html').animate({scrollTop:(Math.round(jQuery(jQuery('body').data('jkNav').selector).eq(jQuery('body').data('inPort').index+1).offset().top)-jQuery('body').data('jkNav').padding)}, {'duration': jQuery('body').data('jkNav').duration, 'easing':'swing', 'complete':function(){jQuery('body').data('jkNavtf', true); jQuery('body').data('jkNav').complete();}});
-	  } else if((jQuery('body').data('inPort').aLength-1)!=jQuery('body').data('inPort').index && jQuery('body').data('inPort').exact==false) {
-	   jQuery('body').data('jkNavtf', false);
-	   jQuery('body, html').animate({scrollTop:(Math.round(jQuery(jQuery('body').data('jkNav').selector).eq(jQuery('body').data('inPort').index).offset().top)-jQuery('body').data('jkNav').padding)}, {'duration':  jQuery('body').data('jkNav').duration, 'easing':'swing', 'complete':function(){jQuery('body').data('jkNavtf', true); jQuery('body').data('jkNav').complete();}});
-	  }
-	 break;
-    }
-   }
-  }
- });
- jQuery('body').data('jkNavtf', true);
+		}
+		if(jQuery.jkNavData.run==true) {
+			if(e.which==jQuery.jkNavData.settings.up || e.which==jQuery.jkNavData.settings.down) {
+				jQuery.jkNavData.inPort={aLength:jQuery(jQuery.jkNavData.settings.selector).length};
+				jQuery(jQuery.jkNavData.settings.selector).each(function(i) {
+					if((jQuery(document).scrollTop()-Math.round(jQuery(this).offset().top))<(0-jQuery.jkNavData.settings.padding)) {
+						jQuery.jkNavData.inPort={exact:false, element:jQuery(this), index:i, aLength:jQuery.jkNavData.inPort.aLength};
+						return false;
+					} else if((jQuery(document).scrollTop()-Math.round(jQuery(this).offset().top))==(0-jQuery.jkNavData.settings.padding)) {
+						jQuery.jkNavData.inPort={exact:true, element:jQuery(this), index:i, aLength:jQuery.jkNavData.inPort.aLength};
+						return false;
+					}
+				});
+				switch(e.which) {
+					case jQuery.jkNavData.settings.up:
+						if(jQuery.jkNavData.inPort.index!=0) {
+							jQuery.jkNavData.run=false;
+							jQuery('body, html').animate({
+								scrollTop:(Math.round(jQuery(jQuery.jkNavData.settings.selector).eq(jQuery.jkNavData.inPort.index-1).offset().top)-jQuery.jkNavData.settings.padding)
+							}, {
+								'duration': jQuery.jkNavData.settings.duration, 
+								'easing':'swing', 
+								'complete':function(){
+									jQuery.jkNavData.run=true;
+									jQuery.jkNavData.settings.complete();
+								}
+							});
+						}
+					break;
+					case jQuery.jkNavData.settings.down:
+						if((jQuery.jkNavData.inPort.aLength-1)!=jQuery.jkNavData.inPort.index && jQuery.jkNavData.inPort.exact==true) {
+							jQuery.jkNavData.jkNavtf=false;
+							jQuery('body, html').animate({
+								scrollTop:(Math.round(jQuery(jQuery.jkNavData.settings.selector).eq(jQuery.jkNavData.inPort.index+1).offset().top)-jQuery.jkNavData.settings.padding)
+							}, {
+								'duration': jQuery.jkNavData.settings.duration,
+								'easing':'swing',
+								'complete':function(){
+									jQuery.jkNavData.run=true;
+									jQuery.jkNavData.settings.complete();
+								}
+							});
+						} else if((jQuery.jkNavData.inPort.aLength-1)!=jQuery.jkNavData.inPort.index && jQuery.jkNavData.inPort.exact==false) {
+							jQuery.jkNavData.run=false;
+							jQuery('body, html').animate({
+								scrollTop:(Math.round(jQuery(jQuery.jkNavData.settings.selector).eq(jQuery.jkNavData.inPort.index).offset().top)-jQuery.jkNavData.settings.padding)
+							}, {
+								'duration': jQuery.jkNavData.settings.duration,
+								'easing':'swing',
+								'complete':function(){
+									jQuery.jkNavData.run=true;
+									jQuery.jkNavData.settings.complete();
+								}
+							});
+						}
+					break;
+				}
+			}
+		}
+	});
+	jQuery.jkNavData.run=true;
 }
